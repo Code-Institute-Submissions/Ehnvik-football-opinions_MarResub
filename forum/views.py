84b9d3c2ac621a4a4a5cfb28ef3d81e09f1a0d8a
Ehnvik <gustav.ehnvik@gmail.com>
2022-01-16 22:10:14 +0000

    add templates and connect django to html files

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+    {
+        'author': 'Gustav',
+        'title': 'Forum Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 16, 2022'
+    },
+    {
+        'author': 'Daniel',
+        'title': 'Forum Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 16, 2022'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'forum/home.html', context)
+
+
+def about(request):
+    return render(request, 'forum/about.html', {'title': 'About'})

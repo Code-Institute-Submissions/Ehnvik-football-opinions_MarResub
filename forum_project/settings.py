038ebb1d6ccf397f30fa3bebf81d2fb47a79b61e
Ehnvik <gustav.ehnvik@gmail.com>
2022-01-18 20:06:16 +0000

    create profile page and fix 404 error when try reach profile while logged out

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -126,6 +126,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'forum-home'
 
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

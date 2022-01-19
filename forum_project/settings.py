9b9cb4cbbae877827fa59fabfbaaeffb0fafed24
Ehnvik <gustav.ehnvik@gmail.com>
2022-01-19 23:35:12 +0000

    upgrade settings file

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import dj_database_url
 
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-474ek6qg*pvc#flc3xcb)3k+dyv224fb@-2iim)gu76*f2&(6p'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-474ek6qg*pvc#flc3xcb)3k+dyv224fb@-2iim)gu76*f2&(6p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['football-opinions.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -88,7 +90,7 @@ DATABASES = {
 }
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://qjeorufhlbvnzt:9e4d73b4f881dbdee294c28d8ebeebe779be6ce2a4ed3db1c292ab6dc6e8358e@ec2-52-31-219-113.eu-west-1.compute.amazonaws.com:5432/dbaovetbdpfhff')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 

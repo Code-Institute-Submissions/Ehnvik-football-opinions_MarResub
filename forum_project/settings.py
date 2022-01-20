c1b1248348fa165975c04edd5094283fcb85b12d
Ehnvik <gustav.ehnvik@gmail.com>
2022-01-20 02:10:57 +0000

    update readme

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-474ek6qg*pvc#flc3xcb)3k+dyv224fb@-2iim)gu76*f2&(6p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['football-opinions.herokuapp.com']
+ALLOWED_HOSTS = ['football-opinions.herokuapp.com', 'localhost']
 
 
 # Application definition

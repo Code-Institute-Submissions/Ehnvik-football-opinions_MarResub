973603d176c875eeb43c4e37a2301650b2d1c593
Ehnvik <gustav.ehnvik@gmail.com>
2022-03-16 21:56:42 +0000

    Deployment part 2

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 import dj_database_url
 
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,12 +82,17 @@ WSGI_APPLICATION = 'forum_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': BASE_DIR / 'db.sqlite3',
+            }
+        }
 
 
 # Password validation

ccd765a83b8ca40a8119679e297ff2affbe7a261
Ehnvik <gustav.ehnvik@gmail.com>
2022-03-16 23:14:46 +0000

    Removed secret key and set debug

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-474ek6qg*pvc#flc3xcb)3k+dyv224fb@-2iim)gu76*f2&(6p'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['football-opinions.herokuapp.com', 'localhost']
 

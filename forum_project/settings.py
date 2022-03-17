b1a21561f18a58b7f76da963bb145a4f64ec6b7f
Ehnvik <gustav.ehnvik@gmail.com>
2022-03-17 18:49:25 +0000

    Added cache control

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -139,6 +139,12 @@ MEDIA_ROOT = Path(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'football-opinions'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
